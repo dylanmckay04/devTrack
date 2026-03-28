b15c6bd44e0a75f031009211fad9c67cb826fe4a
Dylan <dylanmckay8939@gmail.com>
2026-03-28 03:06:20 -0400

    Update websocket.py to use board_events service

--- backend/app/routers/websocket.py
+++ backend/app/routers/websocket.py
@@ -1,25 +1,7 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from typing import List
+from app.services.board_events import manager
 
 router = APIRouter()
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def broadcast(self, message: dict):
-        for connection in self.active_connections:
-            await connection.send_json(message)
-
-
-manager = ConnectionManager()
 
 
 @router.websocket("/ws/board")
@@ -27,8 +9,7 @@ async def board_websocket(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_json()
-            await manager.broadcast(data)
+            await websocket.receive_text()
     except WebSocketDisconnect:
         manager.disconnect(websocket)
 
